ab9bb6695643586039901604db5c1f2aba307aa3
Kevin Breid <kevinbreid@yahoo.com>
2023-07-27 15:50:24 +0200

    delete functionality for cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,5 +1,14 @@
 const Card = ({currency}) => {
 
+    function deleteCard(e){
+        e.preventDefault();
+
+        fetch(`/api/currencies/${currency.id}`, {
+            method: "DELETE"})
+
+
+    }
+
     return (
         <div className="card-cont">
             <p className="card-title">{currency.name}</p>
@@ -9,7 +18,7 @@ const Card = ({currency}) => {
                 <p className="card-stats">{currency.rank}</p>
                 <p className="card-stats">{currency.day_change}</p>
             </div>
-            <p className="card-delete">x</p>
+            <p className="card-delete" onClick={(e) => deleteCard(e)} >x</p>
         </div>
     )
 }
