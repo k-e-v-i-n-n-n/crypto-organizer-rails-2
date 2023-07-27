67f4c746f0341d9fc1d7f4182c1c1b2fb72b531c
Kevin Breid <kevinbreid@yahoo.com>
2023-07-27 15:29:33 +0200

    updating card functionality

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,11 +3,13 @@ const Card = ({currency}) => {
     return (
         <div className="card-cont">
             <p className="card-title">{currency.name}</p>
+            
             <div className="card-stats-cont">
                 <p className="card-stats">{parseFloat(currency?.price).toFixed(2)}</p>
                 <p className="card-stats">{currency.rank}</p>
                 <p className="card-stats">{currency.day_change}</p>
             </div>
+            <p className="card-delete">x</p>
         </div>
     )
 }
